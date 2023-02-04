c5b1662513d37c2081dac743bf687653918ed788
André Mattos <carlosandrematte@gmail.com>
2023-02-04 11:38:07 -0300

    Adjusted setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import find_packages, setup
 
 # Parse the version from the main __init__.py
-with open('__init__.py') as f:
+with open('micasense/__init__.py') as f:
     for line in f:
         if line.find("__version__") >= 0:
             version = line.split("=")[1].strip()
@@ -16,7 +16,7 @@ setup(name='micasense',
       version=version,
       description=u"Micasense Image Processing, altered by GitHub user andrematte to use with RedEdge-P sensor data",
       author=u"MicaSense, Inc. - adapted by andrematte",
-      url='https://github.com/andrematte/micasense',
+      url='https://github.com/andrematte/rededgep-processing',
       license='MIT',
       packages=find_packages(),
       install_requires=[
